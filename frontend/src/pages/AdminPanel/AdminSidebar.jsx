cd4c4b57dc60a9e3953a04655260f0703e23fc71
Dineth Chamupathi Kumara <127776710+ChamupathiDev@users.noreply.github.com>
2025-05-03 12:20:00 +0530

    Update AdminSidebar.jsx

--- frontend/src/pages/AdminPanel/AdminSidebar.jsx
+++ frontend/src/pages/AdminPanel/AdminSidebar.jsx
@@ -40,11 +40,7 @@ const AdminSidebar = ({ isOpen, toggleSidebar }) => {
         {isUserOpen && (
           <ul className="ml-8 text-blue-300">
             
-            <li className='mb-2 py-2 px-3 rounded hover:bg-gray-700 hover:text-white'>
-              <Link to="/users/add" className="flex items-center">
-                <FaChartBar className='w-5 h-5 mr-2' /> Charts
-              </Link>
-            </li>
+            
             <li className='mb-2 py-2 px-3 rounded hover:bg-gray-700 hover:text-white'>
               <Link to="/userdetails" className="flex items-center">
                 <FaTable className='w-5 h-5 mr-2' /> Tables
@@ -63,11 +59,7 @@ const AdminSidebar = ({ isOpen, toggleSidebar }) => {
         </li>
         {isScheduleOpen && (
           <ul className="ml-8 text-blue-300">
-            <li className='mb-2 py-2 px-3 rounded hover:bg-gray-700 hover:text-white'>
-              <Link to="/schedule/reports" className="flex items-center">
-                <FaFileAlt className='w-5 h-5 mr-2' /> Reports
-              </Link>
-            </li>
+            
             <li className='mb-2 py-2 px-3 rounded hover:bg-gray-700 hover:text-white'>
               <Link to="/schedule-chart" className="flex items-center">
                 <FaChartBar className='w-5 h-5 mr-2' /> Charts
@@ -91,16 +83,8 @@ const AdminSidebar = ({ isOpen, toggleSidebar }) => {
         </li>
         {isCourseOpen && (
           <ul className="ml-8 text-blue-300">
-            <li className='mb-2 py-2 px-3 rounded hover:bg-gray-700 hover:text-white'>
-              <Link to="/courses/list" className="flex items-center">
-                <FaFileAlt className='w-5 h-5 mr-2' /> Reports
-              </Link>
-            </li>
-            <li className='mb-2 py-2 px-3 rounded hover:bg-gray-700 hover:text-white'>
-              <Link to="/courses/add" className="flex items-center">
-                <FaChartBar className='w-5 h-5 mr-2' /> Charts
-              </Link>
-            </li>
+            
+            
             <li className='mb-2 py-2 px-3 rounded hover:bg-gray-700 hover:text-white'>
 
               <Link to="/CMhome" className="flex items-center">
